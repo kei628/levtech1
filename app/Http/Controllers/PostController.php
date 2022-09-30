275f918b755c12e36391c358db54d5fab7eff3f6
kei628 <ec2-user@ip-172-31-21-52.ec2.internal>
2022-09-30 23:37:48 +0900

    Add Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,7 @@ class PostController extends Controller
 {
    public function index(Post $post)
     {
-        return $post->get();       //$postの中身を戻り値にする。
+        return view('posts/index')->with(['posts'=> $post->getPaginateByLimit(1)]);      //$=postの中身を戻り値にする。
     }
 }
+?>
