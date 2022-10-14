212a6f93a36689e247c9c7a1bfdf612669a7e29a
kei628 <ec2-user@ip-172-31-21-52.ec2.internal>
2022-10-15 02:16:24 +0900

    ブログ投稿作成処理実行

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
    public function index(Post $post)
     {
-        return view('posts/index')->with(['posts'=> $post->getPaginateByLimit(1)]);      //$=postの中身を戻り値にする。
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);      //$=postの中身を戻り値にする。
     }
     
     public function show(Post $post)
@@ -20,6 +20,13 @@ class PostController extends Controller
     public function create()
     {
         return view('posts/create');
+    }
+    
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
 
